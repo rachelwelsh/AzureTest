42945ff0fc297a9ba51dad33b6dc745d089cc53f
rachelwelsh <rachh.w@live.com>
2016-03-18 15:12:26 +0000

    a

---
+++ week06/errors.js
@@ -0,0 +1,14 @@
+function pain () {
+	var a;
+	var b;
+	var c;
+	
+	a = 10;
+	b = 100;
+	
+	b = a + b;
+	c == a * b;
+	
+	document.writeln(c);
+	console.log(arguments);
+}
---
+++ week06/form.js
@@ -0,0 +1,26 @@
+function main () {
+	var a, b;
+	var c;
+	
+	a = 10;
+	b = 100;
+	
+	b = a + b;
+	c = a * b;
+	
+	document.writeln(c);
+	console.log(arguments);
+}
+
+// function main () {
+// 	var a, b;
+// 	var c;
+// 	
+// 	a = 10;
+// 	b = 100;
+// 	
+// 	b = a + b;
+// 	c = a * b;
+// 	
+// 	document.writeln(c);
+// }
